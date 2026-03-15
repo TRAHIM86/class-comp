ad614140b910818b99a235e953051cbe29ee7608
TRAHIM86 <trahim1986@gmail.com>
2026-03-16 00:00:24 +0300

    feat: set/get localstorage to inputsearch

--- src/assets/components/search-top/search.tsx
+++ src/assets/components/search-top/search.tsx
@@ -4,11 +4,33 @@ import { Btn } from '../../ui/btn';
 import { InputSearch } from '../../ui/inputSearch';
 
 export class Search extends React.Component {
+  state = {
+    inputValue: '',
+  };
+
+  componentDidMount() {
+    const searchStr: string | null = localStorage.getItem('searchStr');
+    console.log(searchStr);
+    this.setState({ inputValue: searchStr });
+  }
+
+  changeInputValue(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    this.setState({ inputValue: value });
+
+    if (typeof value === 'string') {
+      localStorage.setItem('searchStr', value);
+    }
+  }
+
   render() {
     return (
       <section className={search_sec}>
         <form className={search_form}>
-          <InputSearch />
+          <InputSearch
+            value={this.state.inputValue}
+            onChangeFunc={this.changeInputValue}
+          />
           <Btn>SEARCH</Btn>
         </form>
       </section>
--- src/assets/ui/inputSearch.tsx
+++ src/assets/ui/inputSearch.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export class InputSearch extends React.Component {
+interface InputProps {
+  value: string;
+  onChangeFunc: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export class InputSearch extends React.Component<InputProps> {
   render() {
-    return <input name="search" className="border p-2"></input>;
+    return (
+      <input
+        name="search"
+        value={this.props.value}
+        onChange={this.props.onChangeFunc}
+        className="border p-2"
+      ></input>
+    );
   }
 }
