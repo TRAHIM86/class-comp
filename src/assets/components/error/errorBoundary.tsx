0ced6896d4511d06e0cc220eda1f9f32ccb16405
TRAHIM86 <trahim1986@gmail.com>
2026-03-24 00:57:16 +0300

    fix: errorBoundary after chanhed types stste error

--- src/assets/components/error/errorBoundary.tsx
+++ src/assets/components/error/errorBoundary.tsx
@@ -4,15 +4,15 @@ import type { ChildProps, StateError } from '../../../types';
 export class ErrorBoundary extends React.Component<ChildProps, StateError> {
   constructor(props: ChildProps) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { isError: false, errorStatus: '' };
   }
 
   static getDerivedStateFromError(): StateError {
-    return { hasError: true };
+    return { isError: true, errorStatus: 'Render error' };
   }
 
   render(): ReactNode {
-    if (this.state.hasError) {
+    if (this.state.isError) {
       return (
         <div>
           SORRY!!! A simulated rendering error occurred. To remove this text,
