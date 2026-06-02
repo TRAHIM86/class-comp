f3c14795eabdb9bd2ad21f06a52b586ef3ffea84
TRAHIM86 <trahim1986@gmail.com>
2026-06-02 23:55:31 +0300

    fix: replaced class-comm result

--- src/components/result-bottom/results.tsx
+++ src/components/result-bottom/results.tsx
@@ -3,22 +3,22 @@ import { errorMessage, result_sec } from '../../styles/styles';
 import { TableResult } from './tableresult';
 import type { hero, StateError } from '../../types';
 
-export class Result extends React.Component<{
+export const Result = ({
+  heroes,
+  stateError,
+}: {
   heroes: Array<hero>;
   stateError?: StateError;
-}> {
-  render() {
-    return (
-      <section data-testid="result" className={result_sec}>
-        {this.props.stateError?.isError ? (
-          <div className={errorMessage}>
-            Error. Status: {this.props.stateError.errorStatus}. Please try
-            again.
-          </div>
-        ) : (
-          <TableResult heroes={this.props.heroes} />
-        )}
-      </section>
-    );
-  }
-}
+}) => {
+  return (
+    <section data-testid="result" className={result_sec}>
+      {stateError?.isError ? (
+        <div className={errorMessage}>
+          Error. Status: {stateError.errorStatus}. Please try again.
+        </div>
+      ) : (
+        <TableResult heroes={heroes} />
+      )}
+    </section>
+  );
+};
