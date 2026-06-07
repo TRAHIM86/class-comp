29e3a3fdee7e31f09fe221778b002cb9026cb1db
TRAHIM86 <trahim1986@gmail.com>
2026-06-08 00:49:16 +0300

    fix; tests 1

--- src/_tests_/test-utils/dataForTests.tsx
+++ src/_tests_/test-utils/dataForTests.tsx
@@ -7,14 +7,21 @@ export const heroes = [
     gender: 'robot',
     birth_year: '1140',
     height: '100',
+    url: 'https://swapi.py4e.com/api/people/1/',
   },
   {
     name: 'Padme Amidala',
     gender: 'female',
     birth_year: '1190',
     height: '1172',
+    url: 'https://swapi.py4e.com/api/people/3/',
   },
 ];
+
+export const heroesResponse = {
+  countAll: heroes.length,
+  peopleArr: heroes,
+};
 
 // Requests.test ***********************
 export const responseSearchDarth = [
