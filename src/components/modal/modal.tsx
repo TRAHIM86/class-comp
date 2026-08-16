010077e5c289a9d5fee4cc626bd77d1197f5a78a
TRAHIM86 <trahim1986@gmail.com>
2026-08-16 22:35:34 +0300

    feat: added click escape for close modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,8 +2,23 @@ import { createPortal } from 'react-dom';
 import type { ModalProps } from '../../types';
 import { modalOverlay, modalBox } from '../../styles/styles';
 import { Btn } from '../../ui/btn';
+import { useEffect } from 'react';
 
 export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  });
+
   if (!isOpen) return null;
 
   return createPortal(
