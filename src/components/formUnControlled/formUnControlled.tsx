ed8856013f559d71680a94c2bb26f6d282b8bc28
TRAHIM86 <trahim1986@gmail.com>
2026-09-02 21:42:22 +0300

    feat: added addUser function to unControlled form (adding to store user)

--- src/components/formUnControlled/formUnControlled.tsx
+++ src/components/formUnControlled/formUnControlled.tsx
@@ -1,17 +1,32 @@
 import { useRef } from 'react';
 import { modalBtnSend } from '../../styles/styles';
+import { useStore } from '../../store/store';
 
 export const FormUnControled = () => {
   // рефы для полей
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
 
+  // функция добавить юзера в глобальный стор
+  const addUser = useStore((state) => state.addUser);
+
   // функция отправки формы
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log('Name:', nameRef.current?.value);
-    console.log('Email:', emailRef.current?.value);
+    addUser({
+      name: nameRef.current?.value || '',
+      email: emailRef.current?.value || '',
+    });
+    console.log('USERS', useStore.getState().users);
+
+    if (nameRef.current) {
+      nameRef.current.value = '';
+    }
+
+    if (emailRef.current) {
+      emailRef.current.value = '';
+    }
   }
 
   return (
